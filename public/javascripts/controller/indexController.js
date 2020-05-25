060dffb9851847199338e209f81c07e3850de99c
sedatkocaer <sedatkocaer3734@hotmail.com>
2020-05-25 15:48:03 +0300

    new user even wrote

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,15 +1,37 @@
-app.controller('indexController', ['$scope','indexFactory', ($scope,indexFactory) => {
-    
-    const connectionoptions = { 
-        reconnectionAttemps:3,
-        recconnectionDela:600,
+app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
+
+    $scope.messages=[];
+    $scope.init = () => {
+        const username = prompt('please enter the username ');
+
+        if (username)
+            initsocket(username);
+        else
+            return false;
     };
 
-    indexFactory.connectSocket("http://localhost:3000",connectionoptions)
-    .then((socket) => {
-        console.log('bağlantı gerçekleşti.',socket);
-    }).catch((err) => {
-        console.log(err);
-    });
+    function initsocket(username) {
+        const connectionoptions = {
+            reconnectionAttemps: 3,
+            recconnectionDela: 600,
+        };
 
+        indexFactory.connectSocket("http://localhost:3000", connectionoptions)
+            .then((socket) => {
+                socket.emit('newUser',{username});
+                socket.on('userConnectToRoom',(data)=>{
+                    const messageData = {
+                        type:0,// message type info
+                        username:data.username,
+                    };
+                    $scope.messages.push(messageData);
+                    $scope.$apply();
+                });
+
+            }).catch((err) => {
+                console.log(err);
+            });
+    }
+
+    
 }]);
--- src/socketApi.js
+++ src/socketApi.js
@@ -2,11 +2,27 @@ const socketio =require('socket.io');
 const io = socketio();
 
 const socketApi = {};
-
 socketApi.io=io;
+
+const users =[ ];
 
 io.on('connection',(socket)=>{
     console.log("user connected socket");
+
+    socket.on('newUser',(data)=>{
+        const defaultData ={
+            id:socket.id,
+            position:{
+                x:0,
+                y:0
+            }
+        };
+
+        const userData = Object.assign(data,defaultData);
+        users.push(userData);
+        socket.broadcast.emit('userConnectToRoom',userData);
+        
+    });
 });
 
 module.exports=socketApi;
